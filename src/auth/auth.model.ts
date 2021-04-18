00606c0efd0e9329f20af7310f59bf7f480e60d3
Igor Rybyakov <rybyakovigor@gmail.com>
2021-04-18 16:26:04 +0500

    add models

---
+++ src/auth/auth.model.ts
@@ -0,0 +1,4 @@
+export class AuthModel {
+  auth: string;
+  passwordHash: string;
+}
---
+++ src/product/product.model.ts
@@ -0,0 +1,15 @@
+export class ProductModel {
+  image: string;
+  title: string;
+  price: number;
+  credit: number;
+  calculatedRating: number;
+  description: string;
+  advantages: string;
+  disAdvantages: string;
+  categories: string[];
+  tags: string;
+  characteristics: {
+    [key: string]: string;
+  };
+}
